38c28dc6bd03220c4dffad937cd95369ec20c04c
chenjianfang <173775295@qq.com>
2020-09-16 21:28:20 +0800

    修改配置

--- bin/build.js
+++ bin/build.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
+const path = require('path');
 const shell = require('shelljs');
 
 const cwd = process.cwd();
-console.log('cwd: ', cwd);
+const rollupConfigPath = path.join(cwd, 'build/rollup.config.prod.js');
+console.log('rollupConfigPath: ', rollupConfigPath);
 
 shell.cd(cwd);
-shell.exec("npx rollup --config /build/rollup.config.prod.js");
+shell.exec(`npx rollup --config ${rollupConfigPath}`);
--- bin/dev.js
+++ bin/dev.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
+const path = require('path');
 const shell = require('shelljs');
 
 const cwd = process.cwd();
-console.log('cwd: ', cwd);
+const rollupConfigPath = path.join(cwd, 'build/rollup.config.dev.js');
+console.log('rollupConfigPath: ', rollupConfigPath);
 
 shell.cd(cwd);
-shell.exec("npx nodemon -e js,css,tsx,ts --watch src --ignore node_modules --ignore test --exec \"npx rollup --config /build/rollup.config.dev.js\"");
+shell.exec(`npx nodemon -e js,css,tsx,ts --watch src --ignore node_modules --ignore test --exec \"npx rollup --config ${rollupConfigPath}\"`);
