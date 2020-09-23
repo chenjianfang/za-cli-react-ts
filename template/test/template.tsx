005e4bcb9f70fd2d9c9abbe3d2353d14a70fcc09
chenjianfang <173775295@qq.com>
2020-09-23 10:43:24 +0800

    增加版本号

--- template/test/template.tsx
+++ template/test/template.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Index from '../src/index';
-
-function App() {
-    return Index('<p>你好啊哦</p>');
-}
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('app'),
-);
-
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// import Index from '../src/index';
+//
+// function App() {
+//     return Index('<p>你好啊哦</p>');
+// }
+//
+// ReactDOM.render(
+//     <App/>,
+//     document.getElementById('app'),
+// );
+//
